3bd2e6e5b6ec458efc5c528c150dd430e4ba9b34
mulwa <mulwatech@gmail.com>
2017-06-20 20:41:37 +0300

    added angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,19 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { AngularFireModule } from 'angularfire2';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireDatabase } from 'angularfire2/database';
+export  const firebaseConfig = {
+    apiKey: "AIzaSyD5JWb6IBcDJMIai9Y1LiwKKrnpiNSj7ec",
+    authDomain: "itmonitor-bad15.firebaseapp.com",
+    databaseURL: "https://itmonitor-bad15.firebaseio.com",
+    projectId: "itmonitor-bad15",
+    storageBucket: "itmonitor-bad15.appspot.com",
+    messagingSenderId: "1032860006145"
+  };
+
 
 @NgModule({
   declarations: [
@@ -21,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,6 +45,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+items: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+    this.items = db.list('/items');
 
-  constructor(public navCtrl: NavController) {
-
+     console.log(this.items);
   }
+
 
 }
