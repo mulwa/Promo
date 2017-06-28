fc2dd58ebb881ce57b6e9c391bdd460577ee6375
mulwa <mulwatech@gmail.com>
2017-06-28 16:53:05 +0300

    add loading controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController,NavParams } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 import {MyProvider} from '../../providers/my/my';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import {MoredetailsPage}  from '../moredetails/moredetails';
@@ -11,16 +12,25 @@ import {MoredetailsPage}  from '../moredetails/moredetails';
 export class HomePage {
    private eventsList :any;
 
-  constructor(public navCtrl: NavController, private navParams: NavParams, public providerService: MyProvider) {
-    this.providerService.getEventList().subscribe(res=>{
-      console.log(res);
-      this.eventsList = res;
-      console.log(this.eventsList);
+  constructor(public navCtrl: NavController, private navParams: NavParams,
+    public loadingCtrl:LoadingController,  public providerService: MyProvider) {
+    let loader = this.loadingCtrl.create({
+      content: 'Wait fetching Events....',
+    });
+    loader.present().then(()=>{
+      this.providerService.getEventList().subscribe(res=>{
+        this.eventsList = res;
+        loader.dismiss();
+      });
+
+
 
     });
+
   }
   loadMore(event){
     console.log("clicked "+event.eventName);
     this.navCtrl.push(MoredetailsPage,event);
   }
+
 }
