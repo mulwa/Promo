a5e1253bfd4bd9ff24c7b0143fd66fe30888c610
mulwa <mulwatech@gmail.com>
2017-06-29 18:07:32 +0300

    more details

--- src/pages/moredetails/moredetails.ts
+++ src/pages/moredetails/moredetails.ts
@@ -9,15 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MoredetailsPage {
   private event_name:string;
   private event_description:string;
+  private event_venue :string;
+  private event_time: string;
+  private event_target: string;
+  private event_date: string;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MoredetailsPage');
     this.event_name = this.navParams.get("eventName");
     this.event_description = this.navParams.get('eventDescription');
+    this.event_date = this.navParams.get('eventDate');
+    this.event_time = this.navParams.get('eventTime');
+    this.event_venue = this.navParams.get('eventVenue');
 
     console.log("name from second  page "+this.event_name);
   }
